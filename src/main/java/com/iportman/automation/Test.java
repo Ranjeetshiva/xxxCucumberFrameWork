a2948e205240abcca19c66086faab12da1d0bdca
ranjeet <shivaranjeetm@gmail.com>
2021-07-03 15:10:12 +0530

    add pom shared by srirama

--- src/main/java/com/iportman/automation/Test.java
+++ src/main/java/com/iportman/automation/Test.java
@@ -1,4 +1,6 @@
 package com.iportman.automation;
 
 public class Test {
+
+
 }
--- src/test/java/CukesTestRunner.java
+++ src/test/java/CukesTestRunner.java
@@ -1,15 +1,20 @@
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+
+
+//Cucumber API
 import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features= "src/test/resources/features",
         glue="StepDefinitions",
+        tags="@JuneRelease",
         plugin = {"pretty",
-                "html:target/cucumber-reports/cucumber.html",
-                "json:target/cucumber-reports/cucumber.json",
-                "junit:target/cucumber-reports/cucumber.xml"
+                "html:target/cucumber-reports",
+                "json:target/cucumber.json"
         }
 
 )
